851439cb1e7add880c871e1108d8e20ac10d0d31
WakelessSloth56 <wsloth56@foxmail.com>
2022-03-27 20:13:10 +0800

    feat(config): IEConfig

---
+++ src/main/java/org/auioc/mods/itemexporter/config/IEConfig.java
@@ -0,0 +1,12 @@
+package org.auioc.mods.itemexporter.config;
+
+import org.auioc.mods.arnicalib.api.java.holder.BooleanHolder;
+import org.auioc.mods.itemexporter.api.IELanguageList;
+
+public final class IEConfig {
+
+    public static final IELanguageList LANGUAGES = new IELanguageList();
+
+    public static final BooleanHolder MINECRAFT_TAG_ONLY = new BooleanHolder(true);
+
+}
