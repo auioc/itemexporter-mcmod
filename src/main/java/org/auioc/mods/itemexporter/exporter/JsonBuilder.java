c09933d0fb0c1b1f413454d767d7ca4f1b7c8c8e
WakelessSloth56 <wsloth56@foxmail.com>
2022-03-28 15:47:30 +0800

    feat(json): support export creative tabs of item

--- src/main/java/org/auioc/mods/itemexporter/exporter/JsonBuilder.java
+++ src/main/java/org/auioc/mods/itemexporter/exporter/JsonBuilder.java
@@ -3,6 +3,7 @@ package org.auioc.mods.itemexporter.exporter;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import org.auioc.mods.itemexporter.config.IEConfig;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
@@ -15,6 +16,7 @@ public class JsonBuilder {
         json.addProperty("registry_name", item.getRegistryName().toString());
         json.add("display_name", buildDisplayNameJson(item));
         json.add("tags", buildTagJson(item));
+        json.add("creative_tabs", buildCreativeTabJson(item));
 
         return json;
     }
@@ -38,6 +40,19 @@ public class JsonBuilder {
             .filter((tag) -> IEConfig.MINECRAFT_TAG_ONLY.get() ? tag.getNamespace().equals("minecraft") : true)
             .map(ResourceLocation::toString)
             .forEach(json::add);
+
+        return json;
+    }
+
+    private static JsonArray buildCreativeTabJson(Item item) {
+        var json = new JsonArray();
+
+        item.getCreativeTabs()
+            .stream()
+            .map((tab) -> (TranslatableComponent) tab.getDisplayName())
+            .map(TranslatableComponent::getKey)
+            .map((name) -> name.replaceFirst("^itemGroup\\.", ""))
+            .forEach(((name) -> json.add(name)));
 
         return json;
     }
