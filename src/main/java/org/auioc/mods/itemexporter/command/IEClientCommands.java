544f1847ee4901f47cb4eaf87a6f9d0414b38c96
WakelessSloth56 <wsloth56@foxmail.com>
2022-03-29 15:38:03 +0800

    feat(command): feedback message prefix

--- src/main/java/org/auioc/mods/itemexporter/command/IEClientCommands.java
+++ src/main/java/org/auioc/mods/itemexporter/command/IEClientCommands.java
@@ -5,6 +5,7 @@ import com.mojang.brigadier.tree.CommandNode;
 import org.auioc.mods.arnicalib.client.command.AHClientCommands;
 import org.auioc.mods.arnicalib.common.command.impl.VersionCommand;
 import org.auioc.mods.arnicalib.utils.game.CommandUtils.CommandFeedbackHelper;
+import org.auioc.mods.arnicalib.utils.game.TextUtils;
 import org.auioc.mods.itemexporter.ItemExporter;
 import org.auioc.mods.itemexporter.command.impl.ConfigCommand;
 import org.auioc.mods.itemexporter.command.impl.ExportCommand;
@@ -18,10 +19,10 @@ public class IEClientCommands {
 
     private static final CommandNode<CommandSourceStack> NODE = literal(ItemExporter.MOD_ID).build();
 
-    public static final CommandFeedbackHelper FEEDBACK_HELPER = new CommandFeedbackHelper(ItemExporter::i18n);
+    public static final CommandFeedbackHelper FEEDBACK_HELPER = new CommandFeedbackHelper(TextUtils.I18nText(ItemExporter.i18n("command.prefix")), ItemExporter::i18n);
 
     public static void register(final RegisterClientCommandsEvent event) {
-        NODE.addChild(literal("version").executes((ctx) -> VersionCommand.getModVersion(ctx, ItemExporter.MAIN_VERSION, ItemExporter.FULL_VERSION, ItemExporter.MOD_NAME)).build());
+        NODE.addChild(literal("version").executes((ctx) -> VersionCommand.getModVersion(ctx, ItemExporter.class)).build());
 
         NODE.addChild(ConfigCommand.NODE);
         NODE.addChild(ExportCommand.NODE);
